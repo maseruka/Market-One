14102b48180f327c759036bd1b88e5bc41ade3d7
maseruka <masesisaac@gmail.com>
2017-05-27 15:52:25 +0300

    Added local mysql support

--- server/server.js
+++ server/server.js
@@ -5,29 +5,53 @@ var app = express();
 var http = require("http").createServer(app);
 var bodyParser = require('body-parser');
 var mysql = require("mysql");
+var pool  =  mysql.createPool({
+    connectionLimit : 10,
+    host     : process.env.MYSQL_URL,
+    user     : process.env.MYSQL_USER,
+    password : process.env.MYSQL_PASS,
+    database : process.env.MYSQL_DB,
+    debug    :  false
+});  
+
+
 var functions = require("./custom_modules/reusable_functions.js");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-var placest = {
-    origin: {
-        lat: 0.381511, 
-        lng: 32.568406
-    },
-    destination: {
-        lat: 0.360151,
-        lng: 32.562285
-    }
-}
-
-functions.getDistance(placest, (distance)=>{
-    console.log(distance)
-});
-
 app.get('/', (req, res)=>{
-  res.send('Works.');
+  res.send('Works '+USR);
 });
+
+app.post('/login', (req, res)=>{
+  console.log('aa');
+  pool.query('SELECT * FROM admins WHERE email = '+USR+' ', function(err, rows){
+    if(err){ return; console.log(err)}
+      if(rows.length > 0){
+        console.log('asas');
+        res.send('Available');
+    }else{
+      pool.query('INSERT INTO admins SET ?'+req.body, function(err, res) {
+        console.log(err);
+        console.log(JSON.stringify(res));
+      });
+    }
+  })
+});
+
+app.post('/createBizz', (req, res)=>{
+   var name = req.body.name;
+   var slogan = req.body.slogan;
+   console.log(name+' '+slogan);
+
+   pool.query('INSERT INTO businesses SET ?', req.body, function(err, ok) {
+     if (err) {console.log(err); return;}
+       console.log(ok);
+   })
+
+
+})
 
 app.get('/search', (req, res)=>{
   var product = req.body.productSearched;
